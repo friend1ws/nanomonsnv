cda6745fcf468a6d56f2db52d59624ceff9db3e6
ken0-1n <ubuntu@ip-172-31-34-116.ap-northeast-1.compute.internal>
2020-03-10 03:32:14 +0000

    modify ggplot script

--- script/precision_recall4.py
+++ script/precision_recall4.py
@@ -14,7 +14,7 @@ def tpr(lvT, relevant_element):
 def ppv(lvT, lvF):
     return len(lvT) / (len(lvT)+len(lvF))
     
-def get_presigion_recall(variant_file, group, hout, relevant_element=42993):
+def get_presigion_recall(variant_file, group, hout, anno_idx, relevant_element=42993):
 
     l_valid_T = []
     l_valid_F = []
@@ -22,7 +22,7 @@ def get_presigion_recall(variant_file, group, hout, relevant_element=42993):
         for line in hin:
             line = line.rstrip('\n')
             F = line.split('\t')
-            if F[38] == "True":
+            if F[anno_idx] == "True":
                 l_valid_T.append(1)
             else:
                 l_valid_F.append(1)
@@ -65,7 +65,7 @@ def filter_result(in_file, out_file, filter_type):
             print('\t'.join(F), file = hout)
     hout.close()
 
-def annotate_anno(variant_file, output_file, ccl_tabix):
+def annotate_anno(variant_file, output_file, ccl_tabix, chrom_idx, pos_idx, ref_idx, alt_idx):
 
     target_rnames_ccl = []
     tabix_cmd = ['tabix','-l',ccl_tabix]
@@ -94,12 +94,15 @@ def annotate_anno(variant_file, output_file, ccl_tabix):
             nanosnv_record = line
             F = line.split('\t')
             # chrom = "chr"+F[0]
-            chrom = F[0]
-            pos = F[1]
-            ref = F[2]
-            alt = F[3]
-            if ccl_tabix is not None:
+            chrom = F[chrom_idx]
+            pos = F[pos_idx]
+            ref = F[ref_idx]
+            alt = F[alt_idx]
             
+            if ref == "-" or alt == "-": continue
+            
+            if ccl_tabix is not None:
+
                 called_by = False
                 if chrom in target_rnames_ccl:
                     for record_line in ccl_tabix_db.fetch(chrom, int(pos) - 1, int(pos) + 1):
@@ -137,19 +140,46 @@ def annotate_anno(variant_file, output_file, ccl_tabix):
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 ccl_tabix = sys.argv[3]
-
+illumina_file = sys.argv[4]
+illumina_file2 = sys.argv[5]
 
 cmd = ["sort", "-k", "11", "-r", "-n", input_file]
 with open(output_file+".sorted.txt", 'w') as hout:
     subprocess.check_call(cmd, stdout=hout)
 
-annotate_anno(output_file+".sorted.txt", output_file+".anno.txt", ccl_tabix)
+cmd = ["sort", "-k", "18", "-r", "-n", illumina_file]
+with open(output_file+".sorted.illumina.txt", 'w') as hout:
+    subprocess.check_call(cmd, stdout=hout)
 
+if illumina_file2 != "None":
+    cmd = ["sort", "-k", "18", "-r", "-n", illumina_file2]
+    with open(output_file+".sorted.illumina2.txt", 'w') as hout:
+        subprocess.check_call(cmd, stdout=hout)
+
+annotate_anno(output_file+".sorted.txt", output_file+".anno.txt", ccl_tabix, 0,1,2,3)
+annotate_anno(output_file+".sorted.illumina.txt", output_file+".anno.illumina.txt", ccl_tabix, 0,1,3,4)
+if illumina_file2 != "None":
+    annotate_anno(output_file+".sorted.illumina2.txt", output_file+".anno.illumina2.txt", ccl_tabix, 0,1,3,4)
+    
 shutil.copy(output_file+".anno.txt", output_file+".filt1.txt")
 filter_result(output_file+".anno.txt", output_file+".filt2.txt", 2)
 filter_result(output_file+".anno.txt", output_file+".filt3.txt", 3)
 
 with open(output_file, 'w') as hout:
-    get_presigion_recall(output_file+".filt1.txt","Unfiltered", hout)
-    get_presigion_recall(output_file+".filt2.txt","Filtered", hout)
-    get_presigion_recall(output_file+".filt3.txt","Filtered panel-of-normal", hout)
+    get_presigion_recall(output_file+".filt1.txt","Method1", hout, 38)
+    get_presigion_recall(output_file+".filt2.txt","Method2", hout, 38)
+    get_presigion_recall(output_file+".filt3.txt","Method3", hout, 38)
+    get_presigion_recall(output_file+".anno.illumina.txt","Illumina", hout, 31)
+    if illumina_file2 != "None":
+        get_presigion_recall(output_file+".anno.illumina2.txt","Illumina(subsampled)", hout, 31)
+
+os.remove(output_file+".sorted.txt")
+os.remove(output_file+".sorted.illumina.txt")
+os.remove(output_file+".anno.txt")
+os.remove(output_file+".anno.illumina.txt")
+os.remove(output_file+".filt1.txt")
+os.remove(output_file+".filt2.txt")
+# os.remove(output_file+".filt3.txt")
+if illumina_file2 != "None":
+    os.remove(output_file+".sorted.illumina2.txt")
+    os.remove(output_file+".anno.illumina2.txt")
